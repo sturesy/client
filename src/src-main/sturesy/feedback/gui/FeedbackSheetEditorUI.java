a3c83e7a2fae914650d354bc2c87c6612239dadd
Henrik Friedrichsen <henrik@affekt.org>
2014-05-06 00:36:30 +0200

    use splitpanel to display panels left and right

--- src/src-main/sturesy/feedback/gui/FeedbackSheetEditorUI.java
+++ src/src-main/sturesy/feedback/gui/FeedbackSheetEditorUI.java
@@ -1,9 +1,40 @@
 package sturesy.feedback.gui;
 
+import javax.swing.JSplitPane;
+
 import sturesy.core.ui.SFrame;
 
 public class FeedbackSheetEditorUI extends SFrame
 {
 	private static final long serialVersionUID = 1640976438402064082L;
-
+	
+	private JSplitPane _splitpane;
+	private FeedbackQuestionPanelUI _qpanel;
+	private FeedbackQuestionEditorPanelUI _epanel;
+	
+	public FeedbackSheetEditorUI()
+	{
+		super();
+		_splitpane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		_qpanel = new FeedbackQuestionPanelUI();
+		_epanel = new FeedbackQuestionEditorPanelUI();
+		
+		_splitpane.setLeftComponent(_qpanel);
+		_splitpane.setRightComponent(_epanel);
+		
+		add(_splitpane);
+	}
+	
+	public void setDividerLocation(int location)
+	{
+		if(location == -1) // no location set thus far
+			_splitpane.setDividerLocation(30);
+		else
+			_splitpane.setDividerLocation(location);
+	}
+	
+	public int getDividerLocation()
+	{
+		return _splitpane.getDividerLocation();
+	}
 }
